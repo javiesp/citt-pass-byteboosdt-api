4c60157660a1e7174b760cf8559cd96f40692824
javiesp <espinajaviera1312@gmail.com>
2024-06-09 21:41:22 -0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as process from 'process'
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT, '0.0.0.0');
+
+  // Configura las opciones CORS si deseas habilitarlas para cualquier dominio
+  const corsOptions = {
+    origin: '*', // Permite cualquier dominio
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+  };
+
+  app.enableCors(corsOptions);
+
+  // Escucha en el puerto proporcionado por env o en el puerto predeterminado (3000)
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
+
 bootstrap();
